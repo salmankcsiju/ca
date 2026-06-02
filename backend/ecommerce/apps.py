008fabc90532cee69cd05da39a0d5581858c7f1b
salmankcsiju <salmankcsiju@gmail.com>
2026-06-02 13:24:05 -0700

    clean: remove obsolete root-level files and folders, keeping only the ca directory

--- backend/ecommerce/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig # type: ignore
-
-class EcommerceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ecommerce'
-    def ready(self):
-        import ecommerce.signals
