f561cf32e55c8d88b6333fb9a0da43af26d71229
salmankcsiju <salmankcsiju@gmail.com>
2026-06-04 16:34:58 -0700

    feat: create superuser programmatically via django data migration 0006

--- ca/backend/core/settings.py
+++ ca/backend/core/settings.py
@@ -31,7 +31,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'api.middleware.StartupMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
