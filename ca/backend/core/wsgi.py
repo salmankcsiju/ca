9c042627c2184fbcb1e00c0a205fbec8a88a1f72
salmankcsiju <salmankcsiju@gmail.com>
2026-06-04 16:49:19 -0700

    fix(backend): run migrations and seeding programmatically in wsgi/asgi on startup

--- ca/backend/core/wsgi.py
+++ ca/backend/core/wsgi.py
@@ -14,3 +14,14 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
+
+try:
+    from django.core.management import call_command
+    call_command('migrate', interactive=False)
+    
+    from api.models import Product
+    if Product.objects.count() == 0:
+        call_command('seed')
+except Exception as e:
+    print(f"Error during WSGI startup migration/seed: {e}")
+
